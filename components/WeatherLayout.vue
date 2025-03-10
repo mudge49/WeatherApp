31eb75865ffc1b43a83142eaf6f693d55969645c
Michael M. Uy-Oco Jr. <22-1-00710@vsu.edu.ph>
2025-03-10 11:22:08 +0800

    background added

--- components/WeatherLayout.vue
+++ components/WeatherLayout.vue
@@ -5,7 +5,7 @@
   const { weatherData, fetchWeather } = useWeather()
   
   onMounted(() => {
-    fetchWeather('auto:ip')
+    fetchWeather('auto:ip') 
   })
   
   function formatTime(localtime: string) {
@@ -14,27 +14,43 @@
   }
   const weatherBgClass = computed(() => {
   if (!weatherData.value) {
-    return 'bg-gray-200'
+    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-UmZ2e59CEDpbymVrp3RerdeyNlyuAUKnUA&s'
   }
   
   const conditionText = weatherData.value.current.condition.text.toLowerCase()
   if (conditionText.includes('rain')) {
-    return 'bg-gradient-to-b from-gray-600 to-gray-800'
+    return 'https://wallpapers.com/images/hd/aesthetic-rain-wkfiz5qfrxlbx4fl.jpg'
   } else if (conditionText.includes('cloud')) {
-    return 'bg-gradient-to-b from-gray-300 to-gray-500'
+    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh6qGRri3Bo_40zeHb7uByM0od5yVtjyu6wQ&s'
   } else if (conditionText.includes('sun') || conditionText.includes('clear')) {
-    return 'bg-gradient-to-b from-blue-400 to-blue-600'
+    return 'https://i.gifer.com/Iqp.gif'
   } else {
-    return 'bg-gray-200'
+    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-UmZ2e59CEDpbymVrp3RerdeyNlyuAUKnUA&s'
+  }
+  })
+
+  const weatherColorClass = computed(() => {
+  if (!weatherData.value) {
+    return 'bg-gray-500'
+  }
+  
+  const conditionText = weatherData.value.current.condition.text.toLowerCase()
+  if (conditionText.includes('rain')) {
+    return 'bg-white '
+  } else if (conditionText.includes('cloud')) {
+    return 'bg-black-100'
+  } else if (conditionText.includes('sun') || conditionText.includes('clear')) {
+    return 'bg-blue-500'
+  } else {
+    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-UmZ2e59CEDpbymVrp3RerdeyNlyuAUKnUA&s'
   }
   })
 </script>
 
 <template>
-    <div :class="['min-h-screen flex items-center justify-center', weatherBgClass]">
-      <div class="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-6">
-        
-        <div class="flex flex-col items-center justify-center bg-white p-6 shadow-md rounded-md">
+    <div :style="{ backgroundImage: `url(${weatherBgClass})` }" class="min-h-screen bg-cover bg-center flex items-center justify-center">
+      <div class="w-full md:max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-6 px-4 md:px-0">
+        <div class="flex flex-col text-white items-center justify-center bg-blue-500 border-white border-8 p-6 shadow-md rounded-md">
           <img
             v-if="weatherData"
             :src="weatherData.current.condition.icon"
@@ -45,18 +61,14 @@
           <p class="text-xl font-semibold mb-2">
             {{ weatherData ? weatherData.current.condition.text : 'Loading...' }}
           </p>
-          
-          <p class="text-gray-500">
-            <span v-if="weatherData">
-              {{ weatherData.current.temp_c }} °C
-            </span>
-          </p>
 
         </div>
   
         <div class="flex flex-col gap-6">
+          
           <div class="flex flex-row md:flex-col items-center justify-around bg-white p-6 shadow-md rounded-md">
-            <div class="flex items-center mb-4 md:mb-6 md:justify-center">
+            <div class="flex items-center md:justify-center">
+
               <img
                 src="https://img.icons8.com/ios-filled/50/null/marker.png"
                 alt="Location Icon"
@@ -65,6 +77,7 @@
               <p class="text-lg font-semibold">
                 {{ weatherData ? weatherData.location.name : 'Loading...' }}
               </p>
+
             </div>
   
             <div class="flex items-center md:justify-center">
